8955ee0aeeda563f52cd6637e24ac3930104b44c
SimonDanisch <sdanisch@gmail.com>
2016-12-14 20:48:25 +0100

    improve speed

--- test/minimal.jl
+++ test/minimal.jl
@@ -12,8 +12,8 @@ end
 
 
 @inline function inner_velocity_one_form(i, velocity, res_psi_hbar)
-    res, psi1, psi2, hbar = res_psi_hbar
-    i2 = mod.(i, res) + 1
+    idx2, res, psi, hbar = res_psi_hbar
+    i2 = (idx2[1][i[1]], idx2[2][i[2]], idx2[3][i[3]])
     @inbounds begin
         psi12  = psi[i[1],  i[2],  i[3]]
         psix12 = psi[i2[1], i[2],  i[3]]
@@ -27,11 +27,10 @@ end
         conj(psi12[2]) .* psi2n
     ) * hbar
 end
-function velocity_one_form!(velocity, res, psi, hbar = 1.0f0)
-    map_idx!(inner_velocity_one_form, velocity, (res, psi, hbar))
+
+function velocity_one_form!(velocity, idx2, res, psi, hbar = 1.0f0)
+    map_idx!(inner_velocity_one_form, velocity, (idx2, res, psi, hbar))
 end
-
-
 
 function gauge_transform!(psi, q)
     broadcast!(psi, psi, q) do psi, q
@@ -42,20 +41,22 @@ end
 
 function div!(f, res, d, velocity)
     ds = inv.(d .^ 2)
-    map_idx!(f) do xyz
-        x, y, z = xyz
-        ix, iy, iz = mod.(xyz - 2, res) + 1
-        v1 = velocity[x,  y,  z]
-        v2 = Vec(
-            velocity[ix, y,  z][1],
-            velocity[x,  iy, z][2],
-            velocity[x,  y, iz][3]
-        )
+    map_idx!(f, ()) do xyz, f, _
+        @inbounds begin
+            x, y, z = xyz
+            ix, iy, iz = mod.(xyz - 2, res) + 1
+
+            v1 = velocity[x,  y,  z]
+            v2 = Vec(
+                velocity[ix, y,  z][1],
+                velocity[x,  iy, z][2],
+                velocity[x,  y, iz][3]
+            )
+        end
         sum((v1 .- v2) .* ds)
     end
     f
 end
-
 
 function poisson_solve(fac, f)
     fc = fft(f)
@@ -64,8 +65,8 @@ function poisson_solve(fac, f)
     fc
 end
 
-function pressure_project!(velocity, psi, fac, f, res, d)
-    velocity_one_form!(velocity, res, psi)
+function pressure_project!(velocity, psi, idx2, fac, f, res, d)
+    velocity_one_form!(velocity, idx2, res, psi)
     div!(f, res, d, velocity)
     q = poisson_solve(fac, f)
     gauge_transform!(psi, q)
@@ -79,6 +80,7 @@ function Normalize!(psi)
     end
     psi
 end
+@inline twotuple(a, b) = (a, b)
 
 function schroedinger_flow!(psi, mask)
     # extract single psi values into tmp arrays stored in obj
@@ -88,7 +90,7 @@ function schroedinger_flow!(psi, mask)
     psi1 .= (*).(psi1, mask)
     psi2 .= (*).(psi2, mask)
     psi1 = ifft!(fftshift(psi1)); psi2 = ifft!(fftshift(psi2));
-    psi .= tuple.(psi1, psi2) # convert back
+    psi .= twotuple.(psi1, psi2) # convert back
     psi
 end
 
@@ -98,28 +100,31 @@ type Particles
     active::Vector{Int}
 end
 
-function StaggeredAdvect(particle, velocity, dt, d, res, gridsize)
-    map!(view(particle.xyz, particle.active)) do p
+function staggered_advect!(particle, velocity, dt, d, res, gridsize)
+    map!(particle) do p
 
         k1 = staggered_velocity(velocity, p, d, gridsize, res)
 
-        k2 = p + k1 .* dt ./ 2
+        k2 = p + k1 .* dt * 0.5f0
         k2 = staggered_velocity(velocity, k2, d, gridsize, res)
 
-        k3 = p + k2 .* dt ./ 2
+        k3 = p + k2 .* dt * 0.5f0
         k3 = staggered_velocity(velocity, k3, d, gridsize, res)
 
         k4 = p + k3 .* dt
         k4 = staggered_velocity(velocity, k4, d, gridsize, res)
 
-        p .+ dt/6 .* (k1 .+ 2*k2 .+ 2*k3 .+ k4)
+        p .+ dt/6f0 .* (k1 .+ 2f0*k2 .+ 2f0*k3 .+ k4)
     end
 end
 
 @inline function staggered_velocity(velocity, point, d, gs, res)
-    p   = mod.(Vec(point), gs)
-    i   = Vec{3, Int}(floor.(p ./ d) + 1)
-    ip  = mod.(i, res) + 1
+    for i=1:3
+        (point[i] > 0 && point[i] < gs[i]) || return point
+    end
+    p   = (%).(Vec(point), gs)
+    i   = Vec{3, Int}(floor.(p ./ d)) + 1
+    ip  = (%).(i, res) + 1
 
     v0  = velocity[i[1], i[2], i[3]]
 
@@ -138,6 +143,7 @@ end
     w   = p - (i - 1) .* d
     w1  = Vec3f0(w[3], w[3], w[2])
     w2  = Vec3f0(w[2], w[1], w[1])
+
     return Point3f0(
         (1 - w1) .* ((1 - w2) .* v0 + w2 .* pp) +
         w1 .* ((1 - w2) .* pp2 + w2 .* vn)
@@ -148,7 +154,7 @@ end
 
 function Base.append!(p::Particles, xyz)
     @assert length(xyz) + p.length < length(p.xyz)
-    range = (p.length+1):(p.length+length(xyz))
+    range = (p.length + 1):(p.length+length(xyz))
     p.xyz[range] = xyz
     p.length = (p.length+length(xyz))
     append!(p.active, range)
@@ -165,19 +171,23 @@ end
 
 dims = (64, 32, 32)
 grid_res = Vec(dims)
-hbar = 0.1f0; dt = 1f0/48f0;
+hbar = 0.1f0; dt = 1f0/70f0;
 grid_size = Vec(4, 2, 2)
 res = Vec(grid_res)
 d = Vec3f0(grid_size ./ grid_res)
+ranges = Vec(ntuple(3) do i
+    linspace(0, grid_size[i], dims[i])
+end)
+idx2 = ntuple(3) do i
+    mod(1:res[i], res[i]) + 1
+end
 
 jet_velocity = Vec3f0(1, 0, 0)
 nozzle_cen = Vec3f0(2-1.7, 1-0.034, 1+0.066)
 nozzle_len = 0.5;
 nozzle_rad = 0.5;
 n_particles = 50
-ranges = Vec(ntuple(3) do i
-    linspace(0, grid_size[i], dims[i])
-end)
+
 
 
 fac = zeros(Float32, dims)
@@ -203,35 +213,10 @@ psi = [(one(Complex64), one(Complex64) * 0.1f0)
 for i=1:dims[1], j=1:dims[2], k=1:dims[3]]
 
 using BenchmarkTools
-
-
-
-@benchmark inner_velocity_one_form($(Vec(1,2,3)), $(velocity), $(res), $(psi), $(1f0))
-function VelocityOneForm(ix,iy, iz, resx, resy, resz, psi1, psi2, hbar=1.0)
-    ixp = mod(ix, resx) + 1;
-    iyp = mod(iy, resy) + 1;
-    izp = mod(iz, resz) + 1;
-    vx = angle(
-        conj(psi1).*view(psi1, ixp,:,:) +
-        conj(psi2).*view(psi2, ixp,:,:)
-    );
-    vy = angle(
-        conj(psi1).*view(psi1,:,iyp,:) +
-        conj(psi2).*view(psi2,:,iyp,:)
-    )
-    vz = angle(
-        conj(psi1).*view(psi1,:,:,izp) +
-        conj(psi2).*view(psi2,:,:,izp)
-    )
-    vx = vx*hbar;
-    vy = vy*hbar;
-    vz = vz*hbar;
-    vx,vy,vz
+idx2 = ntuple(3) do i
+    mod(1:res[i], res[i]) + 1
 end
-psi1 = map(first, psi)
-psi2 = map(last, psi)
-@time VelocityOneForm(1:dims[1],1:dims[2],1:dims[3], res..., psi1, psi2, 1.0)
-@code_llvm map_idx!(inner_velocity_one_form, velocity, res, psi, 1f0)
+
 
 Normalize!(psi)
 
@@ -240,7 +225,7 @@ kvec = jet_velocity ./ hbar
 omega = sum(jet_velocity.^2) / (2*hbar)
 
 for iter = 1:10
-    map_idx!(psi) do xyz
+    map_idx!(psi, ()) do xyz, psi, _
         x, y, z = xyz
         _psi = psi[x, y, z]
         pos = getindex.(ranges, xyz)
@@ -252,7 +237,7 @@ for iter = 1:10
         end
         _psi
     end
-    pressure_project!(velocity, psi, fac, f_tmp, res, d)
+    pressure_project!(velocity, psi, idx2, fac, f_tmp, res, d)
 end
 using GLVisualize, GLAbstraction
 
@@ -274,8 +259,6 @@ newp = Point3f0[begin
 end for i=1:n_particles]
 
 append!(particle, newp)
-
-
 
 
 filter!(particle.active) do i
@@ -308,10 +291,10 @@ for i=1:1000
     # incompressable schroedinger flow
     schroedinger_flow!(psi, mask)
     Normalize!(psi)
-    pressure_project!(velocity, psi, fac, f_tmp, res, d)
+    pressure_project!(velocity, psi, idx2, fac, f_tmp, res, d)
 
     # constrain velocity
-    map_idx!(psi) do xyz
+    map_idx!(psi, ()) do xyz, psi, _
         x, y, z = xyz
         _psi = psi[x, y, z]
         pos = getindex.(ranges, xyz)
@@ -323,7 +306,7 @@ for i=1:1000
         end
         _psi
     end
-    pressure_project!(velocity, psi, fac, f_tmp, res, d)
+    pressure_project!(velocity, psi, idx2, fac, f_tmp, res, d)
     set_arg!(velocity_vis, :rotation, vec(velocity))
 
     newp = Point3f0[begin
@@ -337,31 +320,12 @@ for i=1:1000
 
     append!(particle, newp)
     # advect and show particles
-    velocity_one_form!(velocity, res, psi)
+    velocity_one_form!(velocity, idx2, res, psi)
     velocity .= (.*).(velocity, Scalar(inv.(d)))
     filter!(in_grid, particle.active)
     particle.length = length(particle.active)
-    StaggeredAdvect(particle, velocity, dt, d, grid_res, grid_size)
+    staggered_advect!(view(particle.xyz, particle.active), velocity, dt, d, grid_res, grid_size)
     set_arg!(particle_vis, :position, particle.xyz)
     set_arg!(particle_vis, :indices, map(x-> Cuint(x-1), particle.active))
     yield()
-end
-
-
-
-
-function test(velocity, res, psi, hbar)
-    @inbounds for i=1:
-    i2 = mod.(i, res) + 1
-    psi12  = psi[i[1],  i[2],  i[3]]
-    psix12 = psi[i2[1], i[2],  i[3]]
-    psiy12 = psi[i[1],  i2[2] ,i[3]]
-    psiz12 = psi[i[1],  i[2],  i2[3]]
-    psi1n = Vec(psix12[1], psiy12[1], psiz12[1])
-    psi2n = Vec(psix12[2], psiy12[2], psiz12[2])
-    angle.(
-        conj(psi12[1]) .* psi1n .+
-        conj(psi12[2]) .* psi2n
-    ) * hbar
-end
 end
